c120e0c95b2281d45bbe0279953a2b9606a94ec8
odrevet <odrevet@gmail.com>
2019-02-20 10:25:01 +0100

    better handling of test.c arguments. page is optional and defautls to '/'. Updated docmentation accordingly

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # C_HTTP
 
-Demonstrate how to performe an HTTP request with the the C language using sockets for Linux and Windows. 
+Demonstrate how to performe an HTTP GET request with the the C language using sockets, for Linux and Windows.
+
+Most of the calls are wrapped in http.c
+
+test.c uses  http.c to demonstrate how to actually do a request.
 
 # build
 
@@ -10,14 +14,20 @@ gcc test.c http.c -o http_req
 
 # Usage
 
+`hostname` is mandatory, `page` is optional (defaults to '/')
+
 ```
-./http_req hostname page
+./http_req <hostname> [<page>]
 ```
 
 # example
 
 ```
-./http_req www.example.com "/index.php?id=42&action=foo" 
+./http_req www.example.com "/"
+```
+
+```
+./http_req www.example.com "/index.php?id=42&action=foo"
 ```
 
 * the hostname is not prefixed by 'http://'
--- test.c
+++ test.c
@@ -7,24 +7,25 @@ char* strdup (const char* str);
 
 #define ERR_NODATA "Nothing received after an HTTP request. "
 
-void usage(const char* program_name)
-{
-  printf("usage: %s hostname page\n", program_name);
-  printf("example: %s www.example.com \"/index.php?id=42&action=foo\"\n", program_name);
+void usage(const char* program_name){
+  printf("usage: %s <hostname> [<page>]\n", program_name);
+  puts("`hostname` is mandatory, `page` is optional (defaults to '/')");
+  puts("`is present,`page must start with `/`. Otherwise the response will likely be a `400 - bad request`");
+  printf("example: %s www.example.com /\n", program_name);
 }
 
-//////////////////////////////////////
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
   //check arguments
-  if(argc < 3){
+  if(argc <= 1){
+    puts("Missing arguments. ");
     usage((*argv));
     exit (EXIT_FAILURE);
   }
 
   const char *hostname = *(argv+1);
-  const char *page     = *(argv+2);
+  const char *page = (argc == 2) ? "/" : *(argv+2);
+
+  if(page[0] != '/')puts("Warning: page argument do not starts with `/`\n");
 
   //some networking initialisations
 #if defined (WIN32)
@@ -40,7 +41,7 @@ int main(int argc, char **argv)
 
   if(!ip){
     puts("Cannot resolve hostname's ip");
-    puts("Check your network connection and that you entered the hostname correcly");
+    puts("Check your network connection and that you entered the valid hostname");
     usage(*(argv));
 
     exit(EXIT_FAILURE);

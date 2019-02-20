ae98815190f6a0d2c11167954180e3317a895f7e
odrevet <odrevet@gmail.com>
2019-02-20 09:51:15 +0100

    rewrite http_request function. format code

--- http.c
+++ http.c
@@ -1,7 +1,6 @@
 #include "http.h"
 
-char* host_to_ip(const char* hostname)
-{
+char* host_to_ip(const char* hostname){
   struct hostent *host_entry;
   host_entry=gethostbyname(hostname);
   if(host_entry){
@@ -9,88 +8,81 @@ char* host_to_ip(const char* hostname)
   }
   else return NULL;
 }
- 
-bool http_create_socket(SOCKET *sock, SOCKADDR_IN *sin, const char *ip)
-{
+
+bool http_create_socket(SOCKET *sock, SOCKADDR_IN *sin, const char *ip){
   *sock = socket(AF_INET, SOCK_STREAM, 0);		//init the socket
- 
+
   sin->sin_addr.s_addr = inet_addr(ip);			//init the socket address on ip / port / network
   sin->sin_port = htons(PORT);
   sin->sin_family = AF_INET;
- 
+
   if(connect(*sock, (SOCKADDR*)sin, sizeof(*sin)) == SOCKET_ERROR)return false;
 
 #ifdef linux
   fcntl(*sock, F_SETFL, O_NONBLOCK);
 #elif defined WIN32
   u_long mode=1;
-  ioctlsocket(*sock,FIONBIO,&mode); 
+  ioctlsocket(*sock,FIONBIO,&mode);
 #endif
 
   return true;
- 
+
 }
- 
-char* http_request(SOCKET sock, const char *hostname, const char *page)
-{
+
+char* http_request(SOCKET sock, const char *hostname, const char *page){
   char buf[BUFSIZ];
-  char *result = NULL;
-  int l=0;
+  int len = 0;
   int selection;
 
-  l = sprintf(buf, "GET %s %s\r\n", page, HTTP_VERSION);
-  send(sock, buf, l, 0);
-  buf[0]='\0';
-  l = sprintf(buf, "Host: %s\r\n", hostname);
-  send(sock, buf, l, 0);
-  buf[0]='\0';
-  send(sock, USER_AGENT, strlen(USER_AGENT), 0);
-  send(sock, ENCODE_TYPE, strlen(ENCODE_TYPE), 0);
-  send(sock, CONNECTION_TYPE, strlen(CONNECTION_TYPE), 0);
-  send(sock, "\r\n", strlen("\r\n"), 0);
+  //Create a GET HTTP request
+  len = sprintf(buf, "\
+GET %s %s\r\n\
+Host: %s\r\n\
+%s\r\n\
+\r\n",
+		page, HTTP_VERSION,
+		hostname,
+		CONNECTION_TYPE);
+
+  printf("---------- HTTP REQUEST ---------- \n\
+'%s'\n\
+--------------------------------------------------------------------------------\n",
+	 buf);
+
+  //send the http request
+  send(sock, buf, strlen(buf), 0);
 
   // timeout
-  struct timeval tv; 
-  tv.tv_sec = TIMEOUT_SEC; 
-  tv.tv_usec = TIMEOUT_MSEC; 
- 
-  fd_set fdread; 
-  FD_ZERO(&fdread); 
-  FD_SET(sock,&fdread);  
+  struct timeval tv;
+  tv.tv_sec = TIMEOUT_SEC;
+  tv.tv_usec = TIMEOUT_MSEC;
+
+  fd_set fdread;
+  FD_ZERO(&fdread);
+  FD_SET(sock, &fdread);
+
+  // the result is the remote page content.
+  // will be reallocated at each call of recv
+  char *result = malloc(sizeof(char));
+  result[0] = '\0';
 
   while(1){
-
     selection = select(sock+1, &fdread, NULL, NULL, &tv);
 
-    if (!selection){
-      l=0;
+    if (!selection || !(FD_ISSET(sock, &fdread))){
       break;
     }
     else{
-      if(FD_ISSET(sock, &fdread)){
-        l = recv(sock, buf, BUFSIZ, 0);
-	if(l>0){
-	  buf[l] = '\0';  
-	  if(result){
-	    result = realloc(result, (strlen(result) + l + 1) * sizeof(char));
-	  }
-	  else{
-	    result = malloc((l + 1)* sizeof(char));	 //allocate size of received data + end of string character
-	    result[0] = '\0';
-	  }
-
-	  strncat(result, buf, l);
-	}      
-      }
-
+      len = recv(sock, buf, BUFSIZ, 0);
+      if(len == 0)break;
+      result = realloc(result, (strlen(result) + len) * sizeof(char));
+      strncat(result, buf, len);
     }
   }
-
 
   return result;
 }
 
-char* http_header_strip(char* content)
-{
+char* http_header_strip(char* content){
   return strstr(content, "\r\n\r\n")+4;
 }
--- http.h
+++ http.h
@@ -2,8 +2,8 @@
 #define HTTP_H
 
 /**
-   @summary Networking related function 
-   @author  Olivier DREVET 
+   @summary Networking related function
+   @author  Olivier DREVET
    @date    2011
 */
 
@@ -18,9 +18,9 @@
 #include <fcntl.h>
 
 char* strdup (const char* str);
- 
+
 #ifdef WIN32
-#include <winsock2.h> 
+#include <winsock2.h>
 #elif defined linux
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -41,7 +41,7 @@ typedef struct in_addr IN_ADDR;
 #define PORT		 80
 #define USER_AGENT      "User-Agent: Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.2.10) Gecko/20100922 Ubuntu/11.10 (lucid) Firefox/5.0 GTB7.1\r\n"
 #define ENCODE_TYPE     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/ *;q=0.8\r\n"
-#define CONNECTION_TYPE "Connection: keep-alive\r\n"
+#define CONNECTION_TYPE "Connection: close\r\n"
 #define HTTP_VERSION    "HTTP/1.1"
 #define TIMEOUT_SEC     5
 #define TIMEOUT_MSEC    0
